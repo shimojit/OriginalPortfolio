d55e75e07cc1b4dc73c2a7979d8429891c14dca8
shimojit <0liberty4life@gmail.com>
2023-10-17 13:27:05 +0900

    introページの作成と各種設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  
+  # ルートURL
+  root "home#intro"
+
+
+  # ==============================学習用に以下のルーティングも記載する。==============================
+
+  # get "/", to: "home#intro"
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
