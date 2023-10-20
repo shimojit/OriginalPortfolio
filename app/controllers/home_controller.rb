21d32a3d0101251ea3f4b99c86b54787cee30b2d
shimojit <0liberty4life@gmail.com>
2023-10-20 11:23:24 +0900

    html修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def intro
   end
+
+  def learn
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  root 'home#intro'
   
-  # get 'home/intro'
+  # ルートURL
+  root "home#intro"
+
+  get "/learn", to: "home#learn"
+
+
+  # ==============================学習用に以下のルーティングも記載する。==============================
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # get "/", to: "home#intro"
 end
