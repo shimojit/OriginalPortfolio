94e1feb650895ce1b78cecd6d6d8973cc3b2347f
shimojit <0liberty4life@gmail.com>
2023-10-23 17:58:18 +0900

    react対応

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -2,25 +2,38 @@
 // like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
 // of the page.
 
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import PropTypes from 'prop-types'
+
+// const Hello = props => (
+//   <div>Hello {props.name}!</div>
+// )
+
+// Hello.defaultProps = {
+//   name: 'David'
+// }
+
+// Hello.propTypes = {
+//   name: PropTypes.string
+// }
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   ReactDOM.render(
+//     <Hello name="React" />,
+//     document.body.appendChild(document.createElement('div')),
+//   )
+// })
+
 import React from 'react'
-import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
+import ReactDOM from 'react-dom/client'
 
 const Hello = props => (
   <div>Hello {props.name}!</div>
 )
 
-Hello.defaultProps = {
-  name: 'David'
-}
+const root = ReactDOM.createRoot(document.getElementById('app'));
 
-Hello.propTypes = {
-  name: PropTypes.string
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <Hello name="React" />,
-    document.body.appendChild(document.createElement('div')),
-  )
-})
+root.render(
+  <Hello name="React" />,
+);
