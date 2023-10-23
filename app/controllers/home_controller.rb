fa18e108cd35f9ade74f0f3242cd635fa4c86850
shimojit <0liberty4life@gmail.com>
2023-10-24 00:19:16 +0900

    React導入断念

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,6 @@ class HomeController < ApplicationController
   def intro
   end
 
-  def dummy
-  end
-
   def learn
-  end
-
-  def react
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,4 @@ Rails.application.routes.draw do
   root "home#intro"
 
   get "/learn", to: "home#learn"
-  # get "/learn", to: "home#dummy"
-
-  get "/react", to: "home#react"
-
-
-  # ==============================学習用に以下のルーティングも記載する。==============================
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  # get "/", to: "home#intro"
 end
